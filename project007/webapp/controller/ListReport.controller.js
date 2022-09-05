4c0babfd1a017ec9792cb862947bf6d0d3af5dac
Dzmitry Babichau <dzmitry.babichau@LeverX.local>
2022-09-05 22:25:18 +0300

    added sorting

--- project007/webapp/controller/ListReport.controller.js
+++ project007/webapp/controller/ListReport.controller.js
@@ -7,8 +7,18 @@ sap.ui.define(
     "sap/m/MessageToast",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter",
   ],
-  function (BaseController, JSONModel, Fragment, MessageBox, MessageToast, Filter, FilterOperator) {
+  function (
+    BaseController,
+    JSONModel,
+    Fragment,
+    MessageBox,
+    MessageToast,
+    Filter,
+    FilterOperator,
+    Sorter
+  ) {
     "use strict";
 
     return BaseController.extend("webapp.controller.ListReport", {
@@ -28,7 +38,11 @@ sap.ui.define(
       _setStateModel: function () {
         var oStateModel = new JSONModel({
           StatusButtons: false,
-          EditMode:      false,
+          EditMode: false,
+          sortType: {
+            Name: "sort",
+            ID: "sort",
+          },
         });
 
         this.getView().setModel(oStateModel, "stateModel");
@@ -38,8 +52,9 @@ sap.ui.define(
        * Selects a row.
        */
       onSelectionTable: function () {
-        var oStateModel         = this.getView().getModel("stateModel");
-        var bIsSelectedContexts = this.byId("CategoriesTable").getSelectedContexts();
+        var oStateModel = this.getView().getModel("stateModel");
+        var bIsSelectedContexts =
+          this.byId("CategoriesTable").getSelectedContexts();
 
         oStateModel.setProperty("/StatusButtons", !!bIsSelectedContexts.length);
       },
@@ -97,7 +112,6 @@ sap.ui.define(
         this.navigate("ObjectPageCategory", {
           CategoryId: nCategoryId,
         });
-
       },
 
       /**
@@ -106,7 +120,7 @@ sap.ui.define(
        * @param {number} nIDNewCategory ID new category.
        */
       openDialog: function (aCategories) {
-        var nNewCategoryID = aCategories[aCategories.length-1].ID+1;
+        var nNewCategoryID = aCategories[aCategories.length - 1].ID + 1;
         var oView = this.getView();
         var oODataModel = oView.getModel();
 
@@ -145,12 +159,12 @@ sap.ui.define(
        *
        */
       onOpenDialogCategory: function () {
-        var that        = this;
+        var that = this;
         var oODataModel = this.getView().getModel();
         this.getView().setBusy(true);
 
         oODataModel.read(`/Categories`, {
-          urlParameters: {$orderby: "ID"},
+          urlParameters: { $orderby: "ID" },
           success: function (mData) {
             that.openDialog(mData.results);
           },
@@ -182,7 +196,7 @@ sap.ui.define(
           oODataModel.deleteCreatedEntry(oCtx);
           oDialog.close();
           oView.setBusy(false);
-        })
+        });
       },
 
       /**
@@ -198,11 +212,13 @@ sap.ui.define(
        * Deletes product.
        *
        */
-       onConfirmDeletion: function () {
-        var that              = this;
-        var oODataModel       = this.getView().getModel();
-        var oStateModel       = this.getView().getModel("stateModel");
-        var aSelectedCategory = this.byId("CategoriesTable").getSelectedContexts().map((oCategory) => oCategory.getPath());
+      onConfirmDeletion: function () {
+        var that = this;
+        var oODataModel = this.getView().getModel();
+        var oStateModel = this.getView().getModel("stateModel");
+        var aSelectedCategory = this.byId("CategoriesTable")
+          .getSelectedContexts()
+          .map((oCategory) => oCategory.getPath());
 
         aSelectedCategory.forEach((sPath) => {
           oODataModel.remove(sPath, {
@@ -223,10 +239,14 @@ sap.ui.define(
        */
       onDeleteCategoryButton: function () {
         var that = this;
-        var nSelectedCategory = this.byId("CategoriesTable").getSelectedContexts().length;
+        var nSelectedCategory =
+          this.byId("CategoriesTable").getSelectedContexts().length;
 
         MessageBox.confirm(
-          that.i18n("WarningMessage", nSelectedCategory === 1 ? "Category" : "Categories"),
+          that.i18n(
+            "WarningMessage",
+            nSelectedCategory === 1 ? "Category" : "Categories"
+          ),
           {
             actions: [MessageBox.Action.OK, MessageBox.Action.CANCEL],
             emphasizedAction: MessageBox.Action.OK,
@@ -244,39 +264,98 @@ sap.ui.define(
       },
 
       /**
-      * Combine all filters.
-      */
-       combineFilters: function() {
-        var oStateModel         = this.getView().getModel("stateModel");
-        var sAllFieldValue      = oStateModel.getProperty("/AllField")?.trim();
-        var aCategoryNameValue  = this._getArrayCategories();
-        var oItemsBinding       = this.byId("CategoriesTable").getBinding("items");
-        var aFilters            = [];
+       * Combine all filters.
+       */
+      combineFilters: function () {
+        var oStateModel        = this.getView().getModel("stateModel");
+        var sAllFieldValue     = oStateModel.getProperty("/AllField")?.trim();
+        var aCategoryNameValue = this._getArrayCategories();
+        var oItemsBinding      = this.byId("CategoriesTable").getBinding("items");
+        var aFilters           = [];
 
-        if (sAllFieldValue && !isNaN(sAllFieldValue)){
+        if (sAllFieldValue && !isNaN(sAllFieldValue)) {
           aFilters.push(new Filter("ID", FilterOperator.EQ, sAllFieldValue));
-        } else if (sAllFieldValue){
-          aFilters.push(new Filter("Name", FilterOperator.Contains, sAllFieldValue));
+        } else if (sAllFieldValue) {
+          aFilters.push(
+            new Filter("Name", FilterOperator.Contains, sAllFieldValue)
+          );
         }
 
         if (aCategoryNameValue) {
-          aFilters.push(...aCategoryNameValue.map(sValue => new Filter("Name", FilterOperator.Contains, sValue)));
+          aFilters.push(
+            ...aCategoryNameValue.map(
+              (sValue) => new Filter("Name", FilterOperator.Contains, sValue)
+            )
+          );
         }
-        oItemsBinding.filter(aFilters)
+        oItemsBinding.filter(aFilters);
       },
 
       /**
        * Get an array of suppliers.
-       * 
+       *
        * @returns {Array} Suppliers.
-       * 
+       *
        * @private
        */
       _getArrayCategories: function () {
-       var aSelectedItems = this.byId("Category").getSelectedItems();
-       if(aSelectedItems.length){
-        return aSelectedItems.map(oItem => oItem.getText())
-       }
+        var aSelectedItems = this.byId("Category").getSelectedItems();
+        if (aSelectedItems.length) {
+          return aSelectedItems.map((oItem) => oItem.getText());
+        }
+      },
+
+      /**
+       * Sort products button press event handler.
+       *
+       * @param {string} sPropertyName sorting type.
+       */
+      onSortButtonPress: function (sPropertyName) {
+        var oStateModel   = this.getView().getModel("stateModel");
+        var oItemsBinding = this.byId("CategoriesTable").getBinding("items");
+
+        oItemsBinding.sort(this.getSorter(sPropertyName, oStateModel));
+      },
+
+      /**
+       * Return constructor object.
+       *
+       * @param {string} sPropertyName sorted column name.
+       * @param {object} oStateModel JSON modal.
+       *
+       * @returns Constructor object.
+       */
+      getSorter: function (sPropertyName, oStateModel) {
+        var oSortType = oStateModel.getProperty("/sortType");
+        var oSorter;
+
+        Object.keys(oSortType).forEach(function (sTypeName) {
+          if (sPropertyName === sTypeName) {
+            switch (oSortType[sTypeName]) {
+              case "sort":
+                oStateModel.setProperty(
+                  `/sortType/${sTypeName}`,
+                  "sort-ascending"
+                );
+                oSorter = new Sorter(sPropertyName, false);
+                break;
+              case "sort-ascending":
+                oStateModel.setProperty(
+                  `/sortType/${sTypeName}`,
+                  "sort-descending"
+                );
+                oSorter = new Sorter(sPropertyName, true);
+                break;
+              default:
+                oStateModel.setProperty(`/sortType/${sTypeName}`, "sort");
+                oSorter = null;
+                break;
+            }
+          } else {
+            oStateModel.setProperty(`/sortType/${sTypeName}`, "sort");
+          }
+        });
+        return oSorter;
       },
     });
   }
